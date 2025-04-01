e12853a4c5cea64b606d6df568f4f8bb926b9503
choubaila276 <choubailaallouache66@gmail.com>
2025-04-01 21:26:52 +0000

    add function of retropropagetion

--- Tp/mlp2.py
+++ Tp/mlp2.py
@@ -27,3 +27,23 @@ def propagate(X,W):
 		P += [X]
 	return P
 
+def retroPropagate(X, Y, Weights, alpha=0.05):
+    S = propagate(X, Weights)
+    Deltas = [np.zeros(card) for card in layers_card]
+
+    delta_index = len(Deltas) - 1
+    Deltas[delta_index] = S[delta_index] * (1. - S[delta_index]) * (Y - S[delta_index])
+
+    for delta_index in range(len(Deltas) - 2, -1, -1):
+        S_mul = S[delta_index]
+        W_mul = Weights[delta_index][1:] 
+        Deltas[delta_index] = S_mul * (1. - S_mul) * np.dot(Deltas[delta_index + 1], W_mul.T)
+
+    for Weights_index in range(len(Weights)):
+        Deltas_mul = Deltas[Weights_index + 1]
+        S_mul = np.append(1, S[Weights_index])
+
+        for m in range(len(Weights[Weights_index])):
+            Weights[Weights_index][m] += alpha * Deltas_mul * S_mul[m]
+
+    return Weights
