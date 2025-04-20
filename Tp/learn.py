528b82a198b7fa771cdbd822c27f4f99557e6862
choubaila276 <choubailaallouache66@gmail.com>
2025-04-20 06:34:57 +0100

    Update learn.py modifier le code

--- Tp/learn.py
+++ Tp/learn.py
@@ -1,18 +1,15 @@
 import numpy as np
-from mlp2 import initializer_weights,retroPropagate, propagate
+from mlp2 import initializer_weights, retroPropagate, propagate
 
 data = np.loadtxt("data.txt")
 A = data[:, :-1]
 B = data[:, -1:]
 
 Weights = initializer_weights()
-Weights = initializer_weights()
-for _ in range(10000):
-    for i in range(len(A)):
+
+for i in range(len(A)):
+    for _ in range(2500):
         retroPropagate(A[i], B[i], Weights, alpha=0.1)
 
-
-
-for i in range(5):
-    output = propagate(A[i], Weights)[-1]
-    print(f"Input: {A[i]}, Output: {B[i]}, Expected: {output}")
+    r = propagate(A[i], Weights)[-1]
+    print(f"Input: {A[i]}, Output: {r}, Expected: {B[i]}")
