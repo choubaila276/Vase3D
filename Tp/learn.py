09e4182dca1f28ceac0924556f8cb57550deb302
choubaila276 <choubailaallouache66@gmail.com>
2025-05-04 05:44:23 +0100

    Prédiction sur les données de test

--- Tp/learn.py
+++ Tp/learn.py
@@ -17,7 +17,23 @@ epochs = 20
 for epoch in range(epochs):
     for i in range(len(X_train)):
         retroPropagate(X_train[i], y_train[i], Weights, alpha=0.1)
-      
+
+vase_points = []
+noise_points = []
+
+for i in range(len(X_test)):
+    prediction = propagate(X_test[i], Weights)[-1]
+    print(f"Input: {X_test[i]}, Output: {prediction }")
+
+    predicted_label = 1 if prediction >= 0.5 else 0
+
+    if predicted_label == 1:
+        vase_points.append(X_test[i])
+    else:
+        noise_points.append(X_test[i])
+
+
+
         
 
 
